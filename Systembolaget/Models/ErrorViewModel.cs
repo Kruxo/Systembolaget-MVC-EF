db91a89c7ad800989388647a48978edc3827ee20
Kruxo <142524330+Kruxo@users.noreply.github.com>
2024-12-18 09:15:09 +0100

    init

---
+++ Systembolaget/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Systembolaget.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
