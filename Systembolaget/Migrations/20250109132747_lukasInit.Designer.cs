be8a5f4c35d4f4cb977897ac5256da2bf1aa91f3
Lukas Graff <Lukas.Graff@brightstraining.com>
2025-01-10 09:58:38 +0100

    made login text more friendly and added a link to Login. Added a picture on home site.

--- Systembolaget/Migrations/20250109132747_lukasInit.Designer.cs
+++ Systembolaget/Migrations/20250109132747_lukasInit.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Systembolaget.Models;
@@ -11,9 +12,11 @@ using Systembolaget.Models;
 namespace Systembolaget.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250109132747_lukasInit")]
+    partial class lukasInit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
