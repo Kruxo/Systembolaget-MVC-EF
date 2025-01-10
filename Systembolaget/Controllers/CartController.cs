d90ea5b7f0963722b7e34ddfdcd06aecefcf8257
Kruxo <142524330+Kruxo@users.noreply.github.com>
2025-01-10 10:12:16 +0100

    Cart page

--- Systembolaget/Controllers/CartController.cs
+++ Systembolaget/Controllers/CartController.cs
@@ -1,13 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Systembolaget.Models;
+using Systembolaget.ViewModels;
 
 namespace Systembolaget.Controllers;
 
 public class CartController : Controller
 {
+    private readonly ApplicationDbContext _context;
 
-    public IActionResult Index()
+    public CartController(ApplicationDbContext context)
     {
-        return View();
+        _context = context;
     }
 
+    public async Task<IActionResult> Index()
+    {
+        var viewModel = new CartIndexVm
+        {
+            Products = await _context.Products.ToListAsync()
+        };
+
+        return View(viewModel);
+    }
 }
+
+
+
+
---
+++ Systembolaget/ViewModels/CartIndexVm.cs
@@ -0,0 +1,7 @@
+using Systembolaget.Models;
+
+namespace Systembolaget.ViewModels;
+public class CartIndexVm
+{
+    public List<Product> Products { get; set; } = new List<Product>();
+}
